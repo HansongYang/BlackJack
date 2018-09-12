d4dc7634c098808dc5c319d76be96a8c6b014e95
Hansong Yang <hansongyang@Hansongs-MacBook-Pro.local>
2018-09-11 21:55:32 -0400

    Add some other JUnit test for BlackJack

--- src/test/java/comp3004/JUnitTest.java
+++ src/test/java/comp3004/JUnitTest.java
@@ -44,9 +44,42 @@ public class JUnitTest extends TestCase{
 		game.player.add(card2);
 		
 		assertEquals(true, game.player.split());
-		
+
 		game.player.add(card3);
 		
 		assertEquals(false, game.player.split());
+	}
+	
+	public void testPlayerValue() {
+		BlackJack game = new BlackJack();
+		Card card = new Card(0,12);
+		Card card2 = new Card(1,12);
+		Card card3 = new Card(1,8);
+		game.player.add(card);
+		game.player.add(card2);
+		
+		assertEquals(20, game.player.value());
+		game.player.add2(card3);
+		game.player.add2(card2);
+		assertEquals(19, game.player.value2());
+	}
+	
+	public void testPlayerGetCard() {
+		BlackJack game = new BlackJack();
+		Card card = new Card(0,12);
+		Card card2 = new Card(0,11);
+		game.player.add(card);
+		game.player.add(card2);
+		
+		assertEquals("Correct", "King", game.player.getFirstCard().getRankName());
+		assertEquals("Correct", "Queen", game.player.getLastCard().getRankName());
+		
+		Card card = new Card(0,10);
+		Card card2 = new Card(0,9);
+		game.player.add2(card);
+		game.player.add2(card2);
+		
+		assertEquals("Jack", game.player.getFirstCard().getRankName());
+		assertEquals("10", game.player.getLastCard().getRankName());
 	}
 }
